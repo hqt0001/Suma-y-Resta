1a29e9d43734e0ad759baf38e15b1de323aff612
hqt001 <hqt0001@alu.medac.es>
2026-09-22 08:37:45 +0200

    Primera version de las operciones

---
+++ src/operaciones/Operaciones.java
@@ -0,0 +1,29 @@
+package operaciones;
+
+public class Operaciones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//declarar dos variables de tipo int con valores fijos
+		//calculamos la suma
+		//mostramos el resultado por pantalla
+		
+		int n1= 3;
+		int n2= 7;
+		int n3 = 8;
+		
+		
+		int suma = n1 + n2;
+		System.out.println("La suma es: " + suma);
+		
+		//le restamos 8
+		
+		int resta = suma - n3;
+		System.out.println("La resta es:" + resta);
+		
+		
+	}
+
+}
