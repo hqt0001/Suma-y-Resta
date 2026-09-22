6f0c442b1e66c9bca69908fcc907b5710c3908d2
hqt001 <hqt0001@alu.medac.es>
2026-09-22 08:54:47 +0200

    operaciones v2

--- src/operaciones/Operaciones.java
+++ src/operaciones/Operaciones.java
@@ -13,7 +13,8 @@ public class Operaciones {
 		int n1= 3;
 		int n2= 7;
 		int n3 = 8;
-		
+		int n4 = 4;
+		int n5 = 2;
 		
 		int suma = n1 + n2;
 		System.out.println("La suma es: " + suma);
@@ -23,6 +24,15 @@ public class Operaciones {
 		int resta = suma - n3;
 		System.out.println("La resta es:" + resta);
 		
+		//Lo multiplicamos por 4
+		
+		int multiplicacion = resta * n4;
+		System.out.println("La multiplicacion es: " + multiplicacion);
+		
+		//Lo dividimos entre 2
+		
+		int division = multiplicacion / n5;
+		System.out.println("La division es: " + division);
 		
 	}
 
